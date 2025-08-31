48332b7ef852c101806a1270810c506c8d6439c1
kimiti <kimitikevin@gmail.com>
2025-08-31 22:46:57 +0300

    add dummy data

--- app/src/main/java/com/example/financeapp/Repository/MainRepository.kt
+++ app/src/main/java/com/example/financeapp/Repository/MainRepository.kt
@@ -1,4 +1,12 @@
 package com.example.financeapp.Repository
 
+import com.example.financeapp.Domain.ExpenseDomain
+
 class MainRepository {
+    val items = mutableListOf(
+        ExpenseDomain("Restaurants", 357.15,"img1","jun 2025 19:15"),
+        ExpenseDomain("Restaurants", 357.15,"img2","16 jun 2025 13:57"),
+        ExpenseDomain("Restaurants", 357.15,"img3","16 jun 2025 20:45"),
+        ExpenseDomain("Restaurants", 357.15,"img1","15 jun 2025 22:18")
+    )
 }
